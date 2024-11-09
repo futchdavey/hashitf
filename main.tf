5a55d7e422d66d6a962d3012304ac5bc75034f2c
Dave Toledo <futch_dave@hotmail.com>
2024-11-09 17:48:42 +0800

    Add files via upload
    tf files

---
+++ main.tf
@@ -0,0 +1,44 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">3"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {
+  }
+  subscription_id = "24113a63-a42c-44ef-8d21-076cf9c9cff1"
+}
+
+#Create the Resouce Group
+resource "azurerm_resource_group" "resouce_group" {
+  name     = var.resource_group_name
+  location = var.location
+  tags     = var.tags
+}
+
+resource "azurerm_network_security_group" "network_security_group" {
+  name                = var.name
+  location            = var.location
+  resource_group_name = azurerm_resource_group.resouce_group.name
+
+  dynamic "security_rule" {
+    for_each = var.nsg_rules
+    content {
+      name                       = security_rule.value["name"]
+      priority                   = security_rule.value["priority"]
+      direction                  = security_rule.value["direction"]
+      access                     = security_rule.value["access"]
+      protocol                   = security_rule.value["protocol"]
+      source_port_range          = security_rule.value["source_port_range"]
+      destination_port_range     = security_rule.value["destination_port_range"]
+      source_address_prefix      = security_rule.value["source_address_prefix"]
+      destination_address_prefix = security_rule.value["destination_address_prefix"]
+    }
+  }
+
+  tags = var.tags
+}
